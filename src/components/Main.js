89055cdbe5fcfa4eb886f66167eea66d24d66c34
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-03-16 15:36:11 +0100

    main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,29 @@
+import pic from "./images/firstPic.png";
+import greek from "./images/Greek Salad.png";
 function Main() {
   return (
     <main>
-      <div>
-        <h2>Little Lemon</h2>
-        <h3>Chicago</h3>
-        <p>
-          We are family owned Mediterranean resturant, focused on traditional
-          recipes served with a modern twist
-        </p>
-        <button>Reserve a Table</button>
-      </div>
-      <div>
-        <img />
-      </div>
+      <aside>
+        <div>
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+          <p>
+            We are family owned Mediterranean resturant, focused on traditional
+            recipes served with a modern twist
+          </p>
+          <button>Reserve a Table</button>
+        </div>
+        <div>
+          <img src={pic} alt="main dish pics" />
+        </div>
+      </aside>
+      <article>
+        <h1>This weeks specials!</h1>
+        <button>Online Menu</button>
+        <div>
+          <img src={greek} alt="Greek Salad" />
+        </div>
+      </article>
     </main>
   );
 }
