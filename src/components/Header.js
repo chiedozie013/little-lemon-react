62fa229c6bca0f8e6bcc595473401e7c67057603
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-03-20 16:35:00 +0100

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,21 @@
+import header from "./images/firstPic.png";
+
 function Header() {
-  return <header>header</header>;
+  return (
+    <header>
+      <div className="header">
+        <div className="header-sec">
+          <h1>Little Lemon</h1>
+          <h3 className="header-col">Chicago</h3>
+          <p className="header-col">
+            we are a family owned Mediterranean restaurant, focused on
+            traditional recipes served with a modern twist.
+          </p>
+          <button className="header-button">Reserve a Table</button>
+        </div>
+      </div>
+      <img className="header-photo" src={header} alt="desserts" />
+    </header>
+  );
 }
 export default Header;
