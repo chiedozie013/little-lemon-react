97fca39481a648adc8c7e3ea87018d87cece3ae8
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-03-20 14:33:39 +0100

    updae

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,18 @@
 import logo from "./images/Logo.png";
+import { Routes, Route, Link } from "react-router-dom";
 function Nav() {
   return (
     <nav>
       <img src={logo} alt="" />
+      <Link to="/" className="nav-item">
+        Homepage
+      </Link>
+      <Link to="/about" className="nav-item">
+        About
+      </Link>
+      <Link to="/about" className="nav-item">
+        BookingPage
+      </Link>
       <ul>
         <li>
           <a>Home</a>
