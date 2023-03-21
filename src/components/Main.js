dc0fad155b83813fa7c25c5ca7ed4ad98b1e0eb9
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-03-21 14:13:23 +0100

    update on main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import greek from "./images/Greek Salad.png";
-// import bruchetta from "./images/Bruchetta.png"
-// import lemon from "./images/Lemon Dessert.png"
+import bruchetta from "./images/Bruchetta.png";
+// import lemon from "./images/Lemon Dessert.png";
 
 function Main() {
   return (
@@ -9,7 +9,7 @@ function Main() {
         <h1>This weeks specials!</h1>
         <button className="main-button">Online Menue</button>
       </div>
-      <div>
+      <div className="menu-main">
         <div className="menu">
           <img src={greek} alt="Greek Salad" />
           <h5>
@@ -19,6 +19,28 @@ function Main() {
             The famous grook salad of crispy lettuse, pappers, olives and our
             Chicago style feta cheese garnished with crunchy garlic and rosemary
             croutons.
+          </p>
+          <a href="Order">Order a delivery</a>
+        </div>
+        <div className="menu">
+          <img src={bruchetta} alt="Bruchetta" />
+          <h5>
+            Bruchetta <span>$5.89</span>
+          </h5>
+          <p>
+            Our Bruchetta is made from grilled bread that has been smeared with
+            garlic and seasoned with salt and olive oil.
+          </p>
+          <a href="Order">Order a delivery</a>
+        </div>
+        <div className="menu">
+          <img src={greek} alt="Greek Salad" />
+          <h5>
+            Lemon Dessert <span>$5.00</span>
+          </h5>
+          <p>
+            This comes straight from grandma's recipe book, every last
+            indredient has been courced and is as authentic as can be imagined.
           </p>
           <a href="Order">Order a delivery</a>
         </div>
