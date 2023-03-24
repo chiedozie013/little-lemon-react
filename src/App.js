efa9a8350819565e40db5f618fab9366d227932b
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-03-24 12:50:27 +0100

    update on packages

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
-import Nav from "./components/Nav";
 
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Nav />
       <Header />
       <Main />
       <Footer />
