809af3b32a55613c70e7dbf3f174339bcd6c3f31
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-03-27 12:32:31 +0100

    update on footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -9,6 +9,25 @@ export default function Footer() {
         <div className={classes.footerLogo}>
           <img src={footerLogo} alt="Footer-Logo" />
         </div>
+        <div>
+          <h5>CONTACTt</h5>
+          <menu>
+            <li>
+              <p>No 35 Jamaica steet, New York, USA</p>
+            </li>
+            <li>
+              <p>Give us a call</p>
+              <a>tel: 123-456-7890</a>
+            </li>
+            <li>
+              <p>Write us a mail</p>
+              <a href="malito:info@little-lemon.com"></a>
+            </li>
+            <div>
+              <h5>SOCIAL MEDIA</h5>
+            </div>
+          </menu>
+        </div>
         <Link className={classes.footer}>Home</Link>;
       </div>
     </footer>
