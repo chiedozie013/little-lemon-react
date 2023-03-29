64d42180640616e46e74b74955b0e0d81f0707f2
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-03-29 14:51:11 +0100

    update on hero section

--- src/components/home-page/HomePage.js
+++ src/components/home-page/HomePage.js
@@ -1,11 +1,12 @@
 import { Fragment } from "react";
 import Header from "../../components/layout/Header";
 import Footer from "../../components/layout/Footer";
-
+import HeroSection from "./HeroSection";
 export default function HomePage() {
   return (
     <Fragment>
       <Header />
+      <HeroSection />
       <Footer />
     </Fragment>
   );
