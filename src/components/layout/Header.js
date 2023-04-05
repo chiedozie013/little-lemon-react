f4e42d772f0b8af96f425865f61f5e5818e42329
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-04-05 16:30:37 +0100

    update on nav respnsiveness

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import React, { useState } from "react";
 import Logo from "../../images/Logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
@@ -7,7 +7,7 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import classes from "./Header.module.css";
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   const toggleMenu = () => {
     setIsOpen((prevValue) => !prevValue);
@@ -22,35 +22,37 @@ export default function Header() {
         <div>
           <nav
             className={`${classes.mainNavigation} ${
-              isOpen ? classes.mainNavigationOpen : " "
+              isOpen ? classes.mainNavigationOpen : ""
             }`}
           >
-            {!isOpen && (
-              <menu>
-                <li onClick={toggleMenu}>
-                  <Link to="/">Home</Link>
-                </li>
-                <li onClick={toggleMenu}>
-                  <Link to="/">About</Link>
-                </li>
-                <li onClick={toggleMenu}>
-                  <Link to="/">Menu</Link>
-                </li>
-                <li onClick={toggleMenu}>
-                  <Link to="/">Reservations</Link>
-                </li>
-                <li onClick={toggleMenu}>
-                  <Link to="/">Order Online</Link>
-                </li>
-                <li onClick={toggleMenu}>
-                  <Link to="/">Login</Link>
-                </li>
-              </menu>
-            )}
+            <menu>
+              <li onClick={toggleMenu}>
+                <Link to="/">Home</Link>
+              </li>
+              <li onClick={toggleMenu}>
+                <Link to="/">About</Link>
+              </li>
+              <li onClick={toggleMenu}>
+                <Link to="/">Menu</Link>
+              </li>
+              <li onClick={toggleMenu}>
+                <Link to="/">Reservations</Link>
+              </li>
+              <li onClick={toggleMenu}>
+                <Link to="/">Order Online</Link>
+              </li>
+              <li onClick={toggleMenu}>
+                <Link to="/">Login</Link>
+              </li>
+            </menu>
           </nav>
 
           <button className={classes.toggleButton} onClick={toggleMenu}>
-            <FontAwesomeIcon icon={faBars} />
+            {isOpen ? (
+              <FontAwesomeIcon icon={faBars} />
+            ) : (
+              <FontAwesomeIcon icon={faBars} />
+            )}
           </button>
         </div>
       </div>
