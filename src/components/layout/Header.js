46a5a41c7f17c70ee6ba7d5f124b582926aea060
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-04-05 15:17:54 +0100

    header responsive

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,38 +1,59 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import Logo from "../../images/Logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 import classes from "./Header.module.css";
 
-function Header() {
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prevValue) => !prevValue);
+  };
+
   return (
     <header className={classes.header}>
       <div className={`fluidContainer ${classes.headerContainer}`}>
         <div className={classes.logo}>
           <img src={Logo} alt="Logo" />
         </div>
-        <nav className={classes.mainNavigation}>
-          <menu>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/">About</Link>
-            </li>
-            <li>
-              <Link to="/">Menu</Link>
-            </li>
-            <li>
-              <Link to="/">Reservations</Link>
-            </li>
-            <li>
-              <Link to="/">Order Online</Link>
-            </li>
-            <li>
-              <Link to="/">Login</Link>
-            </li>
-          </menu>
-        </nav>
+        <div>
+          <nav
+            className={`${classes.mainNavigation} ${
+              isOpen ? classes.mainNavigationOpen : " "
+            }`}
+          >
+            {!isOpen && (
+              <menu>
+                <li onClick={toggleMenu}>
+                  <Link to="/">Home</Link>
+                </li>
+                <li onClick={toggleMenu}>
+                  <Link to="/">About</Link>
+                </li>
+                <li onClick={toggleMenu}>
+                  <Link to="/">Menu</Link>
+                </li>
+                <li onClick={toggleMenu}>
+                  <Link to="/">Reservations</Link>
+                </li>
+                <li onClick={toggleMenu}>
+                  <Link to="/">Order Online</Link>
+                </li>
+                <li onClick={toggleMenu}>
+                  <Link to="/">Login</Link>
+                </li>
+              </menu>
+            )}
+          </nav>
+
+          <button className={classes.toggleButton} onClick={toggleMenu}>
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+        </div>
       </div>
     </header>
   );
 }
-export default Header;
