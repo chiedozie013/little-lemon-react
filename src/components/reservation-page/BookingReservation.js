a0acbb8c0bf6f697b91629857ab3ab8d846628a5
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-04-06 11:44:42 +0100

    update on api function

--- src/components/reservation-page/BookingReservation.js
+++ src/components/reservation-page/BookingReservation.js
@@ -1,3 +1,64 @@
+import { Fragment, useEffect, useState, useReducer } from "react";
+
+import { fetchAPI } from "../../api/api";
+import classes from "./BookingReservation.module.css";
+
+export const updateTimes = (state, action) => {
+  switch (action.type) {
+    case "update-times":
+      return fetchAPI(new Date(action.payload));
+    case "reset-times":
+      return fetchAPI(new Date());
+    default:
+      return state;
+  }
+};
+
+export const initializeTimes = () => fetchAPI(new Date());
+
 export default function BookingReservation() {
-  return <section>I am a boy</section>;
+  const [availableTimes, dispatch] = useReducer(updateTimes, initializeTimes());
+  const [occasions, setOccasions] = useState(["Birthday", "Anniversary"]);
+  const [isFormDisplayed, setIsFormDisplayed] = useState(true);
+  const { isLoading, response, submitAPI } = useSubmit();
+  const { onOpen } = useAlertContext();
+
+  useEffect(() => {
+    document.title = "Table reservation | Little Lemon";
+  }, []);
+
+  useEffect(() => {
+    if (response) {
+      if (response.type === "success") {
+        dispatch({ type: "reset-times" });
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+        setIsFormDisplayed(false);
+      }
+      onOpen(response.type, response.message);
+    }
+  }, [response]);
+
+  const updateTimesHandler = (e) => {
+    // console.log(e.target.value);
+    dispatch({ type: "update-times", payload: e.target.value });
+  };
+
+  return (
+    <Fragment>
+      <section className={classes.reservationFormSection}>
+        <div className={`fluidContainer ${classes.reservationPageContainer}`}>
+          <div className={classes.textSection}>
+            <div className={classes.title}>Little Lemon</div>
+            <div className={classes.subtitle}>Chicago</div>
+          </div>
+          <div className={classes.formContainer}>
+            <h3 className="sectionTitle">Make your reservations here...</h3>
+          </div>
+        </div>
+      </section>
+    </Fragment>
+  );
 }
