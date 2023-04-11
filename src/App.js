6d56fde1aba3a0a0e23d7663c6da5f59af46ebf6
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-04-11 12:07:06 +0100

    update on Signup Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 // import SmoothScroll from "./components/SmoothScroll";
 import HomePage from "./components/home-page/HomePage";
 import BookingPage from "./components/reservation-page/ReservationPage";
-import SignUpPage from "./components/sign-up/SignUp";
+import Register from "./components/sign-up/Register";
 import React from "react";
 
 function App() {
@@ -13,10 +13,9 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="/Home-Page" element={<HomePage />} />
         <Route path="/Reservation-Page" element={<BookingPage />} />
-        <Route path="/Sign-Up" element={<SignUpPage />} />
+        <Route path="/Sign-Up" element={<Register />} />
       </Routes>
     </Router>
-    // </SmoothScroll>
   );
 }
 export default App;
