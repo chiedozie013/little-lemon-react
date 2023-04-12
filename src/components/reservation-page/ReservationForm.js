888ca2aac0f95b8a990f2ab839d02d60db57281c
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-04-12 13:56:37 +0100

    update on Reservation form

--- src/components/reservation-page/ReservationForm.js
+++ src/components/reservation-page/ReservationForm.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import {
@@ -12,8 +13,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
-import CustomerDetails from "../sign-up/SignUp";
-
 import classes from "./ReservationForm.module.css";
 
 export default function ReservationForm({
@@ -25,6 +24,10 @@ export default function ReservationForm({
 }) {
   const formik = useFormik({
     initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      number: "",
       indoor: "",
       outdoor: "",
       date: "",
@@ -32,12 +35,17 @@ export default function ReservationForm({
       guests: "",
       // test: 0,
       occasion: "",
+      policy: "",
     },
     onSubmit: (values) => {
       // console.log(values);
       submitAPI(values);
     },
     validationSchema: Yup.object({
+      firstName: Yup.string().required("You have to put your firstname"),
+      lastName: Yup.string().required("You have to put your lastname"),
+      email: Yup.string().required("You have to input a valid email address"),
+      number: Yup.string().required("You have to input your phone number"),
       date: Yup.string().required("You have to pick a date"),
       time: Yup.string().required("You have to pick a time"),
       guests: Yup.number()
@@ -46,19 +54,22 @@ export default function ReservationForm({
         .required("You have to provide at least 1 guest"),
       // test: Yup.string().required(),
       occasion: Yup.string().required("You have to pick an occasion"),
+      policy: Yup.string().required("You have to tick checkbox"),
     }),
   });
 
-  // const getIsFormValid = () => {
-  //   // Implement this function
-  //   return (
-  // {formik.values.date} &&
+  // Validates email address
+  const [setEmail] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(false);
 
-  //     date && time && guests
-  //     // occasion &&
-  //   );
-  // };
+  function handleEmailChange(event) {
+    const inputEmail = event.target.value;
+    setEmail(inputEmail);
 
+    // Check if the input value matches the pattern of a valid email address
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setIsValidEmail(emailPattern.test(inputEmail));
+  }
   return (
     <form onSubmit={formik.handleSubmit} className={classes.form}>
       <FormControl>
@@ -93,6 +104,115 @@ export default function ReservationForm({
             className={classes.formRadio}
           />
         </FormLabel>
+      </FormControl>
+      <FormControl
+        isInvalid={formik.touched.firstName && formik.errors.firstName}
+      >
+        <FormLabel
+          htmlFor="firstName"
+          fontWeight={600}
+          fontSize="var(--font-lead-text)"
+        >
+          First Name
+        </FormLabel>
+        <Input
+          className={classes.input}
+          id="firstName"
+          name="firstName"
+          type="text"
+          placeholder="First name"
+          {...formik.getFieldProps("firstName")}
+        />
+        <FormErrorMessage>
+          {formik.errors.firstName && (
+            <div className={classes.errorContent}>
+              <FontAwesomeIcon icon={faExclamationTriangle} />
+              {formik.errors.firstName}
+            </div>
+          )}
+        </FormErrorMessage>
+      </FormControl>
+      <FormControl
+        isInvalid={formik.touched.lastName && formik.errors.lastName}
+      >
+        <FormLabel
+          htmlFor="lastName"
+          fontWeight={600}
+          fontSize="var(--font-lead-text)"
+        >
+          Last Name
+        </FormLabel>
+        <Input
+          className={classes.input}
+          id="lastName"
+          name="lastName"
+          type="text"
+          placeholder="Last name"
+          {...formik.getFieldProps("lastName")}
+        />
+        <FormErrorMessage>
+          {formik.errors.lastName && (
+            <div className={classes.errorContent}>
+              <FontAwesomeIcon icon={faExclamationTriangle} />
+              {formik.errors.lastName}
+            </div>
+          )}
+        </FormErrorMessage>
+      </FormControl>
+      <FormControl isInvalid={formik.touched.email && formik.errors.email}>
+        <FormLabel
+          htmlFor="email"
+          fontWeight={600}
+          fontSize="var(--font-lead-text)"
+        >
+          Email
+        </FormLabel>
+        <Input
+          className={classes.input}
+          id="email"
+          name="email"
+          type="email"
+          value={formik.values.email}
+          onChange={handleEmailChange}
+          placeholder="you@company.com"
+          {...formik.getFieldProps("email")}
+        />
+        {isValidEmail ? null : (
+          <FormErrorMessage>
+            {formik.errors.email && (
+              <div className={classes.errorContent}>
+                <FontAwesomeIcon icon={faExclamationTriangle} />
+                {formik.errors.email}
+              </div>
+            )}
+          </FormErrorMessage>
+        )}
+      </FormControl>
+      <FormControl isInvalid={formik.touched.number && formik.errors.number}>
+        <FormLabel
+          htmlFor="number"
+          fontWeight={600}
+          fontSize="var(--font-lead-text)"
+        >
+          Phone Number
+        </FormLabel>
+        <Input
+          className={classes.input}
+          id="number"
+          name="number"
+          type="tel"
+          pattern="\+[0-9]{1,3}\s[0-9]{8,10}"
+          placeholder="+123 4567890"
+          {...formik.getFieldProps("number")}
+        />
+        <FormErrorMessage>
+          {formik.errors.number && (
+            <div className={classes.errorContent}>
+              <FontAwesomeIcon icon={faExclamationTriangle} />
+              {formik.errors.number}
+            </div>
+          )}
+        </FormErrorMessage>
       </FormControl>
 
       <FormControl isInvalid={formik.touched.date && formik.errors.date}>
@@ -219,11 +339,27 @@ export default function ReservationForm({
           )}
         </FormErrorMessage>
       </FormControl>
+      <FormControl>
+        <FormLabel
+          htmlFor="comment"
+          fontWeight={600}
+          fontSize="var(--font-lead-text)"
+        >
+          Special Requests
+          <textarea
+            type="textarea"
+            id="comment"
+            name="comment"
+            rows="8"
+            placeholder="Comments"
+            className={classes.input}
+          />
+        </FormLabel>
+      </FormControl>
       <div style={{ marginTop: "1.5rem" }}>
-        {/* <button
+        <button
           className={classes.submitButton}
           fullWidth={true}
-          // disabled={!getIsFormValid()}
           disabled={isLoading}
           isSubmit={true}
         >
@@ -235,9 +371,6 @@ export default function ReservationForm({
           ) : (
             "Submit"
           )}
-        </button> */}
-        <button className={classes.submitButton} onClick={<CustomerDetails />}>
-          Submit
         </button>
       </div>
     </form>
